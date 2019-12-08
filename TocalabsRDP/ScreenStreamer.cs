1122ea192e55ea88b559a9bd45ab1e506730e337
AaronJThompson <aaronjthompson98@gmail.com>
2019-12-08 10:54:27 +0000

    Add ScreenStreamer class skeleton

---
+++ TocalabsRDP/ScreenStreamer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing.Imaging;
+
+namespace TocalabsRDP
+{
+    class ScreenStreamer
+    {
+        private ScreenCapture screenCapture;
+        private SocketClient socket;
+
+        public ScreenStreamer(string host)
+        {
+
+        }
+    }
+}
