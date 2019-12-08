8f1aa2efab2d9c9e53bb6fced7f5b03cc2dd6fcb
AaronJThompson <aaronjthompson98@gmail.com>
2019-12-08 15:29:01 +0000

    Add comments to ScreenCapture

--- TocalabsRDP/ScreenCapture.cs
+++ TocalabsRDP/ScreenCapture.cs
@@ -15,13 +15,16 @@ namespace TocalabsRDP
 
         public ScreenCapture()
         {
+            // initialize with our primary screen resolution
             this.resolution = Screen.PrimaryScreen.Bounds;
         }
 
         public Bitmap Capture()
         {
+            // Initialize an empty bitmap with our resolution and format
             Bitmap capture = new Bitmap(this.resolution.Width, this.resolution.Height, PixelFormat.Format32bppArgb);
             Graphics captureGraphics = Graphics.FromImage(capture);
+            // Copy all of the screen
             captureGraphics.CopyFromScreen(this.resolution.Left, this.resolution.Top, 0, 0, this.resolution.Size);
 
             return capture;
