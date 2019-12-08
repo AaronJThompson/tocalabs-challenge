0b0a647316545551707a21af18a3fe3ef53395fa
AaronJThompson <aaronjthompson98@gmail.com>
2019-12-08 15:45:20 +0000

    Save capture every 5 seconds

--- RDPServer/src/websocket/index.js
+++ RDPServer/src/websocket/index.js
@@ -1,15 +1,21 @@
 const WebSocket = require('ws');
+const fs = require('fs');
 
 class RDPController {
   constructor(server) {
     this.server = server;
     this.init();
     this.frames = 0;
+    this.lastImage = null;
   }
 
   init() {
     this.server.on('connection', this.onConnection.bind(this));
     setInterval(this.outputFPS.bind(this), 1000);
+    setInterval(() => {
+      console.log("Saving last capture...");
+      fs.writeFileSync(__dirname + "../../last_capture.jpg", this.lastImage);
+    }, 5000)
   }
 
   outputFPS() {
@@ -25,6 +31,7 @@ class RDPController {
 
   onMessage(m) {
     this.frames += 1;
+    this.lastImage = m;
   }
 
   sendKey(k) {
