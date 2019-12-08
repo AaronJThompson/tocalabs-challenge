19105b0cb10092d0da8331efa2e26f4a4fbbf48c
AaronJThompson <aaronjthompson98@gmail.com>
2019-12-08 16:33:36 +0000

    Add args to rdp client

--- TocalabsRDP/Program.cs
+++ TocalabsRDP/Program.cs
@@ -13,8 +13,9 @@ namespace TocalabsRDP
         /// </summary>
         static void Main(string[] args)
         {
+            String host = args.Length == 0 ? "ws://localhost:8080" : args[0];
             // Hardcode the server adress for now.
-            ScreenStreamer streamer = new ScreenStreamer("ws://localhost:8080");
+            ScreenStreamer streamer = new ScreenStreamer(host);
             streamer.Toggle();
             var endlessTask = new TaskCompletionSource<bool>().Task;
             endlessTask.Wait();
