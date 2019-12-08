1fcc8451512563b6bb6eb0560c375b037f9aedf5
AaronJThompson <aaronjthompson98@gmail.com>
2019-12-08 12:41:24 +0000

    Add test keypress code

--- RDPServer/src/server.js
+++ RDPServer/src/server.js
@@ -3,7 +3,12 @@ const WebSocket = require('ws');
 const wss = new WebSocket.Server({ port: 8080 });
 
 wss.on('connection', function connection(ws) {
+  console.log("New connection");
   ws.on('message', function incoming(message) {
     console.log(message);
   });
+  ws.send(JSON.stringify({
+    EventType: "key",
+    KeyCode: 91
+  }));
 });
