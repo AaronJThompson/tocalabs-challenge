a000f08f9b5bd3cc7d2b5505f4e7dedfa46d7185
AaronJThompson <aaronjthompson98@gmail.com>
2019-12-08 10:13:30 +0000

    Add test code for saving capture

--- TocalabsRDP/Form1.cs
+++ TocalabsRDP/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,17 @@ namespace TocalabsRDP
 {
     public partial class Form1 : Form
     {
+        private ScreenCapture sc;
         public Form1()
         {
             InitializeComponent();
+            this.sc = new ScreenCapture();
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            Bitmap cap = this.sc.Capture();
+           cap.Save(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\Capture.jpg", ImageFormat.Jpeg);
         }
     }
 }
