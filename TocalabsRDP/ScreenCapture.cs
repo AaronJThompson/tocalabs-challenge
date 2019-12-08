2efd82d575efd55240aee91b7db31dca7e0504b0
AaronJThompson <aaronjthompson98@gmail.com>
2019-12-08 09:52:55 +0000

    Create ScreenCapture class

---
+++ TocalabsRDP/ScreenCapture.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing.Imaging;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace TocalabsRDP
+{
+    class ScreenCapture
+    {
+        private Rectangle resolution = Screen.PrimaryScreen.Bounds;
+    }
+}
