c60a4a324771d7773398312d2f1d449ec98b73d0
AaronJThompson <aaronjthompson98@gmail.com>
2019-12-08 13:14:41 +0000

    Add sendKey and sendToClients methods

--- RDPServer/src/websocket/index.js
+++ RDPServer/src/websocket/index.js
@@ -2,28 +2,38 @@ const WebSocket = require('ws');
 
 class RDPController {
   constructor(server) {
-    this.clients = [];
     this.server = server;
   }
 
   init() {
-
+    this.server.on('connection', this.onConnection.bind(this));
   }
 
   onConnection(ws) {
-
+    ws.on('message', this.onMessage.bind(this));
   }
 
   onMessage(m) {
-
+    console.log(m);
   }
 
   sendKey(k) {
+    this.sendToClients({
+      EventType: "key",
+      KeyCode: k
+    })
+  }
 
+  sendToClients(data) {
+    this.server.clients.forEach((client, idx) => {
+      if(client.readyState === WebSocket.OPEN) {
+        client.send(JSON.stringify(data));
+      }
+    })
   }
 
   sendMouseMove(x, y) {
-    
+
   }
 }
 
