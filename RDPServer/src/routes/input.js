ac9222857d34d26ae2b35f6ec06708de4a3d48c3
AaronJThompson <aaronjthompson98@gmail.com>
2019-12-08 13:35:00 +0000

    Add mouse_move endpoint

--- RDPServer/src/routes/input.js
+++ RDPServer/src/routes/input.js
@@ -1,13 +1,31 @@
 const express = require('express');
 const router = express.Router();
 
+router.use(express.json());
+
 router.post('/key', async (req, res) => {
   try {
     if (rdpController) {
-      rdpController.sendKey(req.keycode)
+      rdpController.sendKey(req.body.keycode)
+      res.status(201).json({message: "Key sent"});
+    } else {
+      throw Error("No RDPController");
     }
-    res.status(201).json({message: "Key sent"});
   } catch (error) {
-    res.status(500).json({message: "Server error"});
+    res.status(500).json({message: "Server error", error: error.message});
+  }
+})
+
+router.post('/mouse_move', async (req, res) => {
+  try {
+    const { x, y } = req.body;
+    if (rdpController) {
+      rdpController.sendMouseMove(x, y);
+      res.status(201).json({message: "Mouse move sent"});
+    } else {
+      throw Error("No RDPController");
+    }
+  } catch (error) {
+    res.status(500).json({message: "Server error", error: error.message});
   }
 })
