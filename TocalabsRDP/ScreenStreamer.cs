acf2087c7a43fdf2bbf5895c298554ef5f521a84
AaronJThompson <aaronjthompson98@gmail.com>
2019-12-08 11:58:50 +0000

    Handle socket properly

--- TocalabsRDP/ScreenStreamer.cs
+++ TocalabsRDP/ScreenStreamer.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using System.Threading;
 using System.Drawing.Imaging;
 using System.Drawing;
+using System.Diagnostics;
 
 namespace TocalabsRDP
 {
@@ -13,31 +14,36 @@ namespace TocalabsRDP
     {
         private ScreenCapture screenCapture;
         private SocketClient socket;
+        private string host;
         private Thread thread;
         private bool running = false;
 
         public ScreenStreamer(string host)
         {
             screenCapture = new ScreenCapture();
-            socket = new SocketClient(host);
+            this.host = host;
             running = false;
         }
 
         public void StartStream()
         {
+            socket = new SocketClient(host);
             running = true;
             while(running)
             {
                 Bitmap cap = screenCapture.Capture();
                 socket.SendImage(cap);
             }
-            Console.WriteLine("Closing stream gracefully");
+            socket.Close();
+            socket = null;
+            Debug.WriteLine("Closing stream thread gracefully");
         }
 
         public void Toggle()
         {
             if(thread != null)
             {
+                Debug.WriteLine("Starting stream");
                 thread = new Thread(StartStream);
                 thread.IsBackground = true;
                 thread.Start();
@@ -45,9 +51,10 @@ namespace TocalabsRDP
             else
             {
                 running = false;
-                if (thread.Join(200) == false)
+                if (thread.Join(500) == false)
                 {
                     thread.Abort();
+                    Debug.WriteLine("Closing stream thread forcefully");
                 }
                 thread = null;
             }
