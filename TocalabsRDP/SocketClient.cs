5c0b83dfcf469bdef064f34de28b7b3764f80b98
AaronJThompson <aaronjthompson98@gmail.com>
2019-12-08 10:31:18 +0000

    Add WebSocketSharp package and create SocketClient class

---
+++ TocalabsRDP/SocketClient.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebSocketSharp;
+
+namespace TocalabsRDP
+{
+    class SocketClient
+    {
+        private WebSocket wsClient;
+
+        public SocketClient(string host)
+        {
+            this.wsClient = new WebSocket(host);
+        }
+    }
+}
